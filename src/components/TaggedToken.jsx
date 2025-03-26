604ebb65f8b5415bd900c73a1cc5860b55ee30d5
Dr John Vidler <j.vidler@lancaster.ac.uk>
2025-03-26 22:11:15 +0000

    Fixed Token path

--- src/components/TaggedToken.jsx
+++ src/components/TaggedToken.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { Token } from './Tokens/Token';
+import { Token } from './Token';
 
 export function TaggedToken({ text, tag, color, inline, index }) {
     if( inline ) {
