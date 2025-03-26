f64e9c961b048c5e3b3866697c470417a064ebc6
Dr John Vidler <j.vidler@lancaster.ac.uk>
2025-03-26 22:06:08 +0000

    Changed to relative path syntax

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,7 @@
-import AnnotatedParagraph from './AnnotatedParagraph'
-
-export * from AnnotatedParagraph
-export * from ConcordanceList
-export * from NGramList
-export * from TaggedToken
-export * from Token
-export * from ValueToken
-export default AnnotatedParagraph
+export * from './AnnotatedParagraph';
+export * from './NGramList';
+export * from './ConcordanceList';
+export * from './TextBlock';
+export * from './Token';
+export * from './ValueToken';
+export * from './TaggedToken';
