caf4cbaff7e9a439b13513b561a630ffb778b0b7
Dr John Vidler <j.vidler@lancaster.ac.uk>
2025-04-02 18:08:48 +0100

    Removed duplicate export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 export { AnnotatedParagraph } from "./components/AnnotatedParagraph";
 export { ConcordanceLine, ConcordanceList } from "./components/ConcordanceList";
 export { NGram, NGramList } from "./components/NGram";
-export { NGramList } from "./components/NGramList";
 export { TaggedToken } from "./components/TaggedToken";
 export { Token } from "./components/Token";
 export { ValueToken } from "./components/ValueToken";
