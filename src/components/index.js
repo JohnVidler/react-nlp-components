8d3554c6eb778ab7f8c4798ab87f4523c4f07cfc
Dr John Vidler <j.vidler@lancaster.ac.uk>
2025-04-02 18:06:54 +0100

    Simplified the library structure in an attempt to get it to register properly when imported

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export * from './AnnotatedParagraph';
-export * from './NGramList';
-export * from './ConcordanceList';
-export * from './TextBlock';
-export * from './Token';
-export * from './ValueToken';
-export * from './TaggedToken';
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-export * from './components';
+export { AnnotatedParagraph } from "./components/AnnotatedParagraph";
+export { ConcordanceLine, ConcordanceList } from "./components/ConcordanceList";
+export { NGram, NGramList } from "./components/NGram";
+export { NGramList } from "./components/NGramList";
+export { TaggedToken } from "./components/TaggedToken";
+export { Token } from "./components/Token";
+export { ValueToken } from "./components/ValueToken";
